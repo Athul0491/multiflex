f9dd5509c8d5dbb44e2d6fcca95a48105ce2c41e
sktomsi <tomcy48@gmail.com>
2024-06-23 18:06:31 +0530

    hero section web ui ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Image from "next/image";
 import { Button } from "~/components/ui/button";
 import heroImg from "../images/hero-image.jpg";
+import quoteStart from "../../public/quote-start.svg";
+import quoteEnd from "../../public/quote-end.svg";
 
 export default function HomePage() {
   return (
     <>
-      <section>
-        <main className="relative flex h-[calc(100vh-70px)] items-center ">
-          <div className="">
+      <main>
+        <div className="relative flex h-[calc(100vh-70px)] items-start ">
+          <div className="mt-[100px]">
             <div className="">
               <h1 className="text-balance text-[70px] font-extrabold leading-[109%] tracking-tighter">
                 Crafting premium Modular Furniture
@@ -20,6 +23,21 @@ export default function HomePage() {
             <div className="mt-10">
               <Button>Discover More</Button>
             </div>
+            <div className="relative mt-[100px] flex w-[300px] flex-col gap-y-4">
+              <div className="absolute -left-8">
+                <Image src={quoteStart} alt="Quote" />
+              </div>
+              <div>
+                We have been proposing Multiflex as our manufacturers
+                for high-end veneer for over 15 years
+              </div>
+              <div className="absolute -right-3 bottom-8">
+                <Image src={quoteEnd} alt="Quote" />
+              </div>
+              <p className="font-extrabold tracking-tighter">
+                Design Sparks, Interiors and Architecture
+              </p>
+            </div>
           </div>
           <div className="h-[calc(100%-10px)] w-[1000px] bg-black drop-shadow-[24px_24px_rgba(0,0,0,1)]">
             <Image
@@ -28,9 +46,10 @@ export default function HomePage() {
               className=" h-full object-cover"
             />
           </div>
-        </main>
-        <div className="absolute inset-0 -z-20 bg-[url(/lines.svg)] bg-top opacity-50"></div>
-      </section>
+        </div>
+
+        <div className="absolute inset-0 -z-20 bg-[url(/lines.svg)] bg-top opacity-55"></div>
+      </main>
     </>
   );
 }
